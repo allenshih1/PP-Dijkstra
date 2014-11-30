8a97995053b31c555d02b0d83ea19027bbdd5dd8
Yi-Shin Shih <yihshih@cs.nctu.edu.tw>
2014-11-30 18:19:26 +0800

    Parallelize using OpenMP added parallel region and a Makefile

--- dijkstra.c
+++ dijkstra.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
+#include"omp.h"
 
-#define MAX_SIZE 3000
+#define MAX_SIZE 10000
 
 int adj[MAX_SIZE][MAX_SIZE][2];
 int count[MAX_SIZE];
 int distance[MAX_SIZE];
 int flag[MAX_SIZE];
+int nthreads, chunk;
+
+void find_my_min();
 
 int main(void)
 {
 	int c, numOfCase;
 	int n, m, u, v, w;
-	int i, j, k;
+	int i, j, min, k;
 	scanf("%d", &numOfCase);
 
 	for(c = 0; c < numOfCase; c++)
@@ -37,21 +41,60 @@ int main(void)
 		}
 
 		distance[0] = 0;
-		for(i = 0; i < n; i++)
+		omp_set_num_threads(4);
+#pragma omp parallel private(i,j)
 		{
-			int min = 1000000000;
-			for(j = 0; j < n; j++)
-				if(!flag[j] && distance[j] != -1 && distance[j]<min )
+#pragma omp single
+			{
+				nthreads = omp_get_num_threads();
+				chunk = n/nthreads;
+			}
+			int id = omp_get_thread_num();
+			int startv = id * chunk;
+			int endv;
+			if(id == nthreads-1)
+				endv = n;
+			else
+				endv = startv + chunk;
+			for(i = 0; i < n; i++)
+			{
+#pragma omp single
 				{
-					min = distance[j];
-					k = j;
+					min = 1000000000;
+				}
+				int my_min = 1000000000;
+				int my_k;
+				for(j = startv; j < endv; j++)
+					if(!flag[j] && distance[j] != -1 && distance[j]<my_min )
+					{
+						my_min = distance[j];
+						my_k = j;
+					}
+#pragma omp critical
+				if(my_min < min)
+				{
+					min = my_min;
+					k = my_k;
 				}
 
-			for(j = 0; j < count[k]; j++)
-				if(distance[adj[k][j][0]] == -1 ||
-						distance[adj[k][j][0]] > distance[k] + adj[k][j][1])
-					distance[adj[k][j][0]] = distance[k] + adj[k][j][1];
-			flag[k] = 1;
+#pragma omp barrier
+#pragma omp single
+				{
+					chunk = count[k]/nthreads;
+					flag[k] = 1;
+				}
+				int startvr = id * chunk;
+				int endvr;
+				if(id == nthreads-1)
+					endvr = n;
+				else
+					endvr = startvr + chunk;
+				for(j = startvr; j < endvr && j < count[k]; j++)
+					if(distance[adj[k][j][0]] == -1 ||
+							distance[adj[k][j][0]] > distance[k] + adj[k][j][1])
+						distance[adj[k][j][0]] = distance[k] + adj[k][j][1];
+#pragma omp barrier
+			}
 		}
 
 		int max = 0;
