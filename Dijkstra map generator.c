426c8623ee4cd0003e37be4741aa628daa3730d1
Wellis <Wellis@WellisdeMacBook-Pro.local>
2014-12-10 16:32:44 +0800

    minor node link to each other

--- Dijkstra map generator.c
+++ Dijkstra map generator.c
@@ -11,7 +11,7 @@
 #include <time.h>
 
 int main (){
-    int i,j;
+    int i,j,k;
     int numberof_node, maxnode,cluster_in;
     int* node_count;
     int* edge_in_clus;
@@ -34,7 +34,7 @@ int main (){
         totalnode+=node_count[i];
         totaledge+=node_count[i];
         printf("%d have %d node",i,node_count[i]);
-        if(node_count[i]>1)edge_in_clus[i]=rand()%(node_count[i]*(node_count[i]-1)/2); //n point at most n(n-1)/2 edge
+        if(node_count[i]>1)edge_in_clus[i]=(node_count[i]*(node_count[i]-1)/2); //n point at most n(n-1)/2 edge
         else edge_in_clus[i]=0;
         printf(" have %d edge between the nodes\n",edge_in_clus[i]);
         totaledge+=edge_in_clus[i];
@@ -62,13 +62,17 @@ int main (){
         for(j=0;j<node_count[i];j++){
             fprintf(fp,"%d %d %d\n",i,currentstart+j,rand()%1000+1);  ///edge to cluster central
         }
-        for(j=0;j<edge_in_clus[i];j++){
+        for(j=0;j<node_count[i];j++){
+            for (k=j+1;k<node_count[i];k++){
+                fprintf(fp,"%d %d %d\n",currentstart+j,currentstart+k,rand()%1000+1);
             
-            int m=rand()%node_count[i]+currentstart;
+            }
+            
+            /*int m=rand()%node_count[i]+currentstart;
             int n=rand()%node_count[i]+currentstart;
             if (m==n&&n+1<currentstart+node_count[i])n++;
             else if(n+1>currentstart+node_count[i])n--;
-            fprintf(fp,"%d %d %d\n",m,n,rand()%1000+1);  ////edge between point in cluster might repeat
+            fprintf(fp,"%d %d %d\n",m,n,rand()%1000+1);  ////edge between point in cluster might repeat*/
         }
         
         currentstart+=node_count[i];
