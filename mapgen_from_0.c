c3d93aee1d7caac7a839f09736d7b9b3dde704fc
Wellis <Wellis@WellisdeMacBook-Pro.local>
2014-12-19 17:15:26 +0800

    mapgen with each node having max(second arg)

--- mapgen_from_0.c
+++ mapgen_from_0.c
@@ -10,16 +10,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main (int argc ,char** argv){
-    int i,j;
-    int numberof_node, maxnode;
+int main (int argc, char* argv[]){
+    int i,j,k;
+    int numberof_node;
+    int maxnode;
     int* node_count;
     int* edge_in_clus;
+    //printf("Input number of cluster and max nodes in each cluster central point:\n");
+    //scanf("%d %d",&numberof_node,&maxnode);
+    
     numberof_node=atoi(argv[1]);
     maxnode=atoi(argv[2]);
     
-    node_count=(int*)malloc(numberof_node*sizeof(int));
-    edge_in_clus=(int*)malloc(numberof_node*sizeof(int));
+    node_count=(int*)malloc((numberof_node+1)*sizeof(int));
+    edge_in_clus=(int*)malloc((numberof_node+1)*sizeof(int));
     
     int totalnode=0;
     int totaledge=0;
@@ -30,23 +34,17 @@ int main (int argc ,char** argv){
     srand(time(NULL));
     
     for(i=0;i<numberof_node-1;i++){
-        node_count[i]=rand()%maxnode+1;
+        node_count[i]=maxnode;
         totalnode+=node_count[i];
         totaledge+=node_count[i];
         //printf("%d have %d node",i,node_count[i]);
-        if(node_count[i]>1)edge_in_clus[i]=rand()%(node_count[i]*(node_count[i]-1)/2); //n point at most n(n-1)/2 edge
+        if(node_count[i]>1)edge_in_clus[i]=(node_count[i]*(node_count[i]-1)/2); //n point at most n(n-1)/2 edge
         else edge_in_clus[i]=0;
         //printf(" have %d edge between the nodes\n",edge_in_clus[i]);
         totaledge+=edge_in_clus[i];
     }
     node_count[numberof_node-1]=0;
     edge_in_clus[numberof_node-1]=0;
-    
-    /*for(i=0;i<numberof_node;i++){
-        printf("%d: %d %d\n",i,node_count[i],edge_in_clus[i]);
-    }*/
-    
-    FILE *fp,*fi;
     
     
     fprintf(stdout,"1\n");
@@ -62,16 +60,21 @@ int main (int argc ,char** argv){
         for(j=0;j<node_count[i];j++){
             fprintf(stdout,"%d %d %d\n",i,currentstart+j,rand()%1000+1);  ///edge to cluster central
         }
-        for(j=0;j<edge_in_clus[i];j++){
+        for(j=0;j<node_count[i];j++){
+            for (k=j+1;k<node_count[i];k++){
+                fprintf(stdout,"%d %d %d\n",currentstart+j,currentstart+k,rand()%1000+1);
+                
+            }
             
-            int m=rand()%node_count[i]+currentstart;
-            int n=rand()%node_count[i]+currentstart;
-            if (m==n&&n+1<currentstart+node_count[i])n++;
-            else if(n+1>currentstart+node_count[i])n--;
-            fprintf(stdout,"%d %d %d\n",m,n,rand()%1000+1);  ////edge between point in cluster might repeat
+            /*int m=rand()%node_count[i]+currentstart;
+             int n=rand()%node_count[i]+currentstart;
+             if (m==n&&n+1<currentstart+node_count[i])n++;
+             else if(n+1>currentstart+node_count[i])n--;
+             fprintf(fp,"%d %d %d\n",m,n,rand()%1000+1);  ////edge between point in cluster might repeat*/
         }
         
         currentstart+=node_count[i];
     }
-        ////map =map cat a.txt
+    
+    ////map =map cat a.txt
 }
